95102666dbbd364198860bed4737d68fdcc9916f
Lunna Cipher <lunna.devsec@gmail.com>
2024-05-01 22:29:45 -0300

    🔧fix: Dynamic_array agora recebe Dynamic
    isso permite que eu crie um array dinamico de qualquer tipo

--- components/Data_struct/Dynamic_array.java
+++ components/Data_struct/Dynamic_array.java
@@ -1,66 +1,117 @@
 package Data_struct;
 import java.util.Arrays;
 
-public class Dynamic_array {
-    private int[] list = {};
+public class Dynamic_array<Data> {
+    private Object[] list;
 
-    public Dynamic_array(int ... items) {
-        for (int item : items) {
+    @SuppressWarnings("unchecked")
+    public Dynamic_array(Data ... items) {
+        this.list = new Object[0];
+        for (Data item : items) {
             add(item);
         }
     }
 
-    public void add(int item) {
-        int[] temp = Arrays.copyOf(list, list.length + 1);
+    public void add(Data item) {
+        Object[] temp = Arrays.copyOf(list, list.length + 1);
         temp[temp.length - 1] = item;
         list = temp;
     }
 
     public void remove(int index) {
-        int[] temp = new int[list.length - 1];
+        Object[] temp = new Object[list.length - 1];
         
         int auxiliar = 0;
         for (int i = 0; i < list.length; i++){
-            if (i == index) continue; //fail first
+            if (i == index) continue;
             temp[auxiliar] = list[i];
             auxiliar++;
         }
         list = temp;
     }
 
-    public int indexOf(int item) {
+    public int indexOf(Data item) {
         for (int i = 0; i < list.length; i++) {
-            if (list[i] == item) {
+            if (list[i].equals(item)) {
                 return i;
             }
         }
         return -1;
     }
+    
 
-    public boolean contains(int item) {
+    public boolean contains(Data item) {
         return indexOf(item) != -1;
     }
 
-    public int[] get() {
-        return list;
+    @SuppressWarnings("unchecked")
+    public Data[] get() {
+        return (Data[]) list;
+    } 
+
+    @SuppressWarnings("unchecked")
+    public Data get(int index) {
+        if (list.length <= index ) return null;
+        return (Data) list[index];
     }
-    public int get(int index) {
-        if (list.length <= index ) return -1; // fail first
-        return list[index];
+
+    public String toString() {
+        return Arrays.toString(list);
     }
-    
 
     public static void main(String[] args) {
-        
-        Dynamic_array list = new Dynamic_array(1, 2, 3, 4, 5);
-        System.out.println(Arrays.toString(list.get()));
-        list.add(6);
-        System.out.println(Arrays.toString(list.get()));
-        list.remove(2);
-        System.out.println(Arrays.toString(list.get()));
-        System.out.println(list.indexOf(4));
-        System.out.println(list.get(list.indexOf(4)));
-        System.out.println(list.contains(4));
+        testAdd();
+        testRemove();
+        testIndexOf();
+        testContains();
+        testGet();
+        printable();
 
+        /* 
+        // isso é bizarro, n sei o motivo de funcionar assim
+        Dynamic_array<String> lista = new Dynamic_array<String>("1", "2", "3", "4", "5");
+        System.out.println(Arrays.toString(lista.get()));
+        System.out.println(lista.get());
+        System.out.println(lista);
+        */
+    }
+
+    public static void testAdd() {
+        Dynamic_array<String> list = new Dynamic_array<String>("1", "2", "3", "4", "5");
+        list.add("6");
+        String[] expected = {"1", "2", "3", "4", "5", "6"};
+        System.out.println("Add Test: " + Arrays.equals(expected, list.get()));
+    }
+
+    public static void testRemove() {
+        Dynamic_array<String> list = new Dynamic_array<String>("1", "2", "3", "4", "5");
+        list.remove(2);
+        String[] expected = {"1", "2", "4", "5"};
+        System.out.println("Remove Test: " + Arrays.equals(expected, list.get()));
+    }
+
+    public static void testIndexOf() {
+        Dynamic_array<String> list = new Dynamic_array<String>("1", "2", "3", "4", "5");
+        int index = list.indexOf("4");
+        System.out.println("IndexOf Test: " + (index == 3));
+    }
+
+    public static void testContains() {
+        Dynamic_array<String> list = new Dynamic_array<String>("1", "2", "3", "4", "5");
+        boolean contains = list.contains("4");
+        System.out.println("Contains Test: " + contains);
+    }
+
+    public static void testGet() {
+        Dynamic_array<String> list = new Dynamic_array<String>("1", "2", "3", "4", "5");
+        String item = list.get(2);
+        String[] expected = {"1", "2", "3", "4", "5"};
+        System.out.println("Get Test: " + (item.equals("3") && Arrays.equals(list.get(), expected)));
+    }
+
+    public static void printable(){
+        Dynamic_array<String> list = new Dynamic_array<String>("1", "2", "3", "4", "5");
+        String expcted = "[1, 2, 3, 4, 5]";
+        System.out.println("Printable Test: " +  (list.toString().equals(expcted)));
     }
 }
